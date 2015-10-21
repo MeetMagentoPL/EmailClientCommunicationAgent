9ea02c0cf01b396991382d59701d6857bf381b77
Maciej <mmbozek@gmail.com>
2015-10-21 13:24:24 +0200

    Display grid

--- EmailClientCommunicationAgent/Observer/SaveEmailSettingsObserver.php
+++ EmailClientCommunicationAgent/Observer/SaveEmailSettingsObserver.php
@@ -3,26 +3,40 @@
 namespace MMHackaton\EmailClientCommunicationAgent\Observer;
 
 use Magento\Framework\Event\ObserverInterface;
-//use Magento\Framework\ObjectManagerInterface;
 
 class SaveEmailSettingsObserver implements ObserverInterface
 {
-//    private $objectManager;
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
+    
+    /**
+     * @var \MMHackaton\EmailClientCommunicationAgent\Model\EmailSettingsFactory
+     */
     private $emailSettingsFactory;
-    
-    public function __construct(\MMHackaton\EmailClientCommunicationAgent\Model\EmailSettingsFactory $factory)
-    {
+
+    public function __construct(
+        \MMHackaton\EmailClientCommunicationAgent\Model\EmailSettingsFactory $factory,
+        \Psr\Log\LoggerInterface $logger
+    ) {
         $this->emailSettingsFactory = $factory;
+        $this->logger = $logger;
     }
-    
+
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        $userId = $observer->getEvent()->getObject()->getData('user_id');
-        $userData = $observer->getEvent()->getObject()->getData();
-        $model = $this->emailSettingsFactory->create()->load($userId, 'user_id');
-        $model->addData($userData);
-        $model->save();
-        
+        try {
+            $userId = $observer->getEvent()->getObject()->getData('user_id');
+            $userData = $observer->getEvent()->getObject()->getData();
+            $model = $this->emailSettingsFactory->create()->load($userId, 'user_id');
+            $model->addData($userData);
+            $model->save();
+        } catch (\Magento\Framework\Validator\Exception $e) {
+            $this->logger->critical($e->getMessage());
+        }
         return $this;
     }
+
 }
